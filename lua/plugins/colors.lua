e50675c2d63454b9ed35f7337599962993f28516
Edward Chong <82353320+ewkchong@users.noreply.github.com>
2023-10-07 00:24:43 -0700

    transparency business

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,4 +1,7 @@
 require('mini.colors').setup()
+require('rose-pine').setup({
+	disable_background = true,
+})
 
 function ColorMyPencils(color)
 	-- color = color or "catppuccin-latte"
@@ -11,9 +14,31 @@ function ColorMyPencils(color)
 	-- 	day_brightness = 0.3
 	-- })
 
-	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
-	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
+	-- THESE MAKE THINGS TRANSPARENT
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
+	vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+	vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none"})
+	vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
+	
+
+	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none", force = true })
+
+	vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none"})
+
+	-- vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = "none" })
+	-- vim.api.nvim_set_hl(0, "TelescopeResultsNormal", { bg = "none" })
+	-- vim.api.nvim_set_hl(0, "TelescopePreviewNormal", { bg = "none" })
+	--
+	-- vim.api.nvim_set_hl(0, "TelescopePromptBorder", { bg = "none" })
+	-- vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { bg = "none" })
+	-- vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { bg = "none" })
+	--
+	-- vim.api.nvim_set_hl(0, "TelescopePromptTitle", { bg = "none" })
+	-- vim.api.nvim_set_hl(0, "TelescopePreviewTitle", { bg = "none" })
+	-- vim.api.nvim_set_hl(0, "TelescopeResultsTitle", { bg = "none" })
 end
 
 ColorMyPencils()
